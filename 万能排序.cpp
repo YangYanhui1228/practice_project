0bbd2f5a26b462392eaa2bbf8a5c6846e66428c3
YangYanhui1228 <76690224+YangYanhui1228@users.noreply.github.com>
2021-03-18 15:24:19 -0800

    Add files via upload

---
+++ 万能排序.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+template<typename T>
+void Swap(T& a, T& b)
+{
+	T temp = b;
+	T = a;
+	b = temp;
+}
+
+template<typename T>
+void Sort(T* ary, unsigned count, bool BigtoS = true)
+{
+	for (int i = 1; i < count; i++)
+	{
+		for (int j = 1; j < count; j++)
+		{
+			bool bcase = BigtoS ? (ary[j - 1] < ary[j]) : (ary[j - 1] > ary[j]);
+			if (bcase) Swap(ary[j - 1], ary[j]);
+		}
+	}
+
+
+}
+
+
+int main()
+{
+	int a[6]{ 123, 456, 789, 256, 754, 870 };
+	short b[6]{ 123, 456, 789, 256, 754, 870 };
+	Sort(a, 6, false);
+	Sort(b, 6, true);
+	for (int i = 0; i < 6; i++) {
+		std::cout << a[i] << std::endl;
+	}
+	for (int i = 0; i < 6; i++) {
+		std::cout << b[i] << std::endl;
+	}
+
+	return 0;
+
+}
